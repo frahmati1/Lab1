9e7330da858b0448fa689362e8c9d7198c23aa3c
frahmati1 <156142399+frahmati1@users.noreply.github.com>
2024-02-12 18:53:53 +0000

    Add files via upload

---
+++ lab1file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def calculate_age():
+    birth_year = int(input("Enter your birth year: "))
+    current_year = int(input("Enter the current year: "))
+    age = current_year - birth_year
+    print(f"You are {age} years old.")
+
+if __name__ == "__main__":
+    calculate_age()
