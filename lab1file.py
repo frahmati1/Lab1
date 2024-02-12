7b91ff89caca99647ac95a982ddde6b009f0eec2
Farnoosh Rahmati <feriii10x@Farnooshs-MacBook-Air.local>
2024-02-12 15:40:00 -0500

    Add execpt to file1lab.py

--- lab1file.py
+++ lab1file.py
@@ -4,7 +4,9 @@ def calculate_age():
     birth_year = int(input("Enter your birth year: "))
     current_year = int(input("Enter the current year: "))
     age = current_year - birth_year
-    print(f"You are {age} years old.")
+    print(f"You are {age} years old."
+except ValueError:
+    print("Please enter an int")
 
 if __name__ == "__main__":
     calculate_age()
